614c3c2551455c66e5e6a99dd1ea7086d7b01cb9
vantung2102 <vantung2102@gmail.com>
2023-09-07 08:12:53 +0700

    init

---
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -0,0 +1,27 @@
+class OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  def facebook
+    generic_callback('facebook')
+  end
+
+  def google_oauth2
+    generic_callback('google_oauth2')
+  end
+
+  def azure_activedirectory_v2
+    generic_callback('azure_activedirectory_v2')
+  end
+
+  private
+
+  def generic_callback(provider)
+    @user = User.from_omniauth(request.env['omniauth.auth'])
+
+    if @user.persisted?
+      sign_in_and_redirect @user, event: :authentication
+      set_flash_message(:notice, :success, kind: provider.capitalize) if is_navigational_format?
+    else
+      session["devise.#{provider}_data"] = request.env['omniauth.auth'].except('extra')
+      redirect_to new_user_registration_url
+    end
+  end
+end
